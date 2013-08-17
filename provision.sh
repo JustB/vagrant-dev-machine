66c8819c1ed2cda01f904c2608641983cfb9d69c
Giustino Borzacchiello <giustinob@gmail.com>
2013-08-18 00:25:39 +0200

    First commit: initial vagrantfile and provisioning script

---
+++ provision.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+apt-get update -qq
+
+if [ ! -f /var/log/first_install_done ];
+    then
+    # Set default password for root user in MySQL
+    echo mysql-server mysql-server/root_password password niubbo | debconf-set-selections
+    echo mysql-server mysql-server/root_password_again password niubbo | debconf-set-selections
+
+    # Install Vim
+    apt-get install -y vim
+
+    # Apache
+    # ----
+    # Install
+    apt-get install -y apache2 
+
+    echo "ServerName localhost" > /etc/apache2/httpd.conf
+    # Enable mod_rewrite
+    a2enmod rewrite
+    # Enable overrides
+    sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/default
+    # Restart Apache
+    service apache2 restart
+
+    # PHP
+    # ---
+    # Install
+    apt-get install -y php5 php5-cli php5-mysql php5-curl php5-gd php5-fpm libapache2-mod-php5 
+
+    # MySQL
+    # -----
+    # Install
+    apt-get install -y mysql-server mysql-client
+
+    # cURL
+    # ----
+    apt-get install -y curl
+
+    # git
+    # ---
+    apt-get install git-core
+
+    # composer
+    # --------
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
+
+    touch /var/log/first_install_done
+fi
+
+if [ ! -h /var/www ];
+    then
+
+    # Remove /var/www
+    rm -rf /var/www
+    # Create symbolic link with the htdocs folder
+    ln -fs /vagrant/htdocs /var/www
+    
+fi
+
+
+
+
+
+
+#echo mysql-server mysql-server/root_password select "vagrant" | debconf-set-selections
+#
+#echo mysql-server mysql-server/root_password_again select "vagrant" | debconf-set-selections
+#
+#apt-get install -y mysql-server apache2 php5 libapache2-mod-php5 php5-mysql
+#VHOST=$(cat <<EOF
+#<VirtualHost *:80>
+#  DocumentRoot "/vagrant/web"
+#  ServerName localhost
+#  <Directory "/vagrant/web">
+#    AllowOverride All
+#  </Directory>
+#</VirtualHost>
+#EOF
+#)
+#echo "${VHOST}" > /etc/apache2/sites-enabled/000-default
+#sudo a2enmod rewrite
+#service apache2 restart
+#mysql -u root -p"vagrant" -e ";DROP DATABASE test;DROP USER ''@'localhost';CREATE DATABASE gitrepos;GRANT ALL ON gitrepos.* TO gitrepos@localhost IDENTIFIED BY 'gitrepos';GRANT ALL ON gitrepos.* TO gitrepos@'%' IDENTIFIED BY 'gitrepos'"
+#sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
+#service mysql restart
+#apt-get clean
